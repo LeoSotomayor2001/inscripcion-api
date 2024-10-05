ac320175bd327ce81ea2ec913cb7eea72bc9258c
Leodomi <leodomi15@gmail.com>
2024-10-05 14:33:16 -0400

    validando el email en las tablas de usuarios y representantes

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\DB;
 
 class LoginRequest extends FormRequest
 {
@@ -25,23 +26,47 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|email|exists:representantes,email',
+            'email' => [
+                'required',
+                'email',
+                function ($attribute, $value, $fail) {
+                    $userExists = DB::table('users')->where('email', $value)->exists();
+                    $representanteExists = DB::table('representantes')->where('email', $value)->exists();
+
+                    if (!$userExists && !$representanteExists) {
+                        $fail('El correo no existe.');
+                    }
+                },
+            ],
             'password' => 'required',
             'user_type' => 'required|in:profesor,representante,administrador'
         ];
     }
+
+    /**
+     * Get the custom messages for validation errors.
+     *
+     * @return array<string, string>
+     */
     public function messages(): array
     {
         return [
             'email.required' => 'El email es requerido',
             'email.email' => 'El email no es válido',
-            'email.exists' => 'El email no existe',
             'password.required' => 'La contraseña es requerida',
             'user_type.required' => 'El tipo de usuario es requerido',
             'user_type.in' => 'El tipo de usuario no es válido'
-            
         ];
     }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
     protected function failedValidation(Validator $validator)
     {
         throw new ValidationException($validator, response()->json(['errors' => $validator->errors()], 422));
